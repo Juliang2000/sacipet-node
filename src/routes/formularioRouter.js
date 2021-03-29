8000fad0c405dec9cde14469d11d0a9514159a33
Ekimaru <74165503+Ekimaru@users.noreply.github.com>
2021-03-29 14:44:16 -0500

    cambios formularioRouter y fotos

--- src/routes/formularioRouter.js
+++ src/routes/formularioRouter.js
@@ -7,7 +7,7 @@ const adops = require('../controllers/formulario.controller');
 
 
 
-router.post("/formulario", async(req, res) => {
+router.post("/formulario", async (req, res) => {
 
     try {
         /**Se toman solo los campos necesarios 
@@ -54,7 +54,7 @@ router.post("/formulario", async(req, res) => {
             pregunta_adpcion_18,
             terminos,
             id
-    
+
         } = req.body;
 
         /**Se guardan dentro de un array,
@@ -62,161 +62,161 @@ router.post("/formulario", async(req, res) => {
          * de la petición 
          */
         const campos = [{
-                nombre: 'nombre_adoptante',
-                campo: nombre_adoptante
-            },
-            {
-                nombre: 'direccion_adoptante',
-                campo: direccion_adoptante
-            },
-            {
-                nombre: 'id_codigo',
-                campo: id_codigo
-            },
-            {
-                nombre: 'localidad',
-                campo: localidad
-            },
-            {
-                nombre: 'telefono',
-                campo: telefono
-            },
-            {
-                nombre: 'email',
-                campo: email
-            },
-            {
-                nombre: 'ocupacion',
-                campo: ocupacion
-            },
-            {
-                nombre: 'estado_civil',
-                campo: estado_civil
-            },
-            {
-                nombre: 'pregunta_mascota_1',
-                campo: pregunta_mascota_1
-            },
-            {
-                nombre: 'pregunta_mascota_2',
-                campo: pregunta_mascota_2
-            },
-            {
-                nombre: 'pregunta_mascota_3',
-                campo: pregunta_mascota_3
-            },
-            {
-                nombre: 'pregunta_mascota_4',
-                campo: pregunta_mascota_4
-            },
-            {
-                nombre: 'pregunta_familia_1',
-                campo: pregunta_familia_1
-            },
-            {
-                nombre: 'pregunta_familia_2',
-                campo: pregunta_familia_2
-            },
-            {
-                nombre: 'pregunta_familia_3',
-                campo: pregunta_familia_3
-            },
-            {
-                nombre: 'pregunta_familia_4',
-                campo: pregunta_familia_4
-            },
-            {
-                nombre: 'pregunta_familia_5',
-                campo: pregunta_familia_5
-            },
-            {
-                nombre: 'pregunta_familia_6',
-                campo: pregunta_familia_6
-            },
-            {
-                nombre: 'pregunta_familia_7',
-                campo: pregunta_familia_7
-            },
-            {
-                nombre: 'pregunta_adpcion_1',
-                campo: pregunta_adpcion_1
-            },
-            {
-                nombre: 'pregunta_adpcion_2',
-                campo: pregunta_adpcion_2
-            },
-            {
-                nombre: 'pregunta_adpcion_3',
-                campo: pregunta_adpcion_3
-            },
-            {
-                nombre: 'pregunta_adpcion_4',
-                campo: pregunta_adpcion_4
-            },
-            {
-                nombre: 'pregunta_adpcion_5',
-                campo: pregunta_adpcion_5
-            },
-            {
-                nombre: 'pregunta_adpcion_6',
-                campo: pregunta_adpcion_6
-            },
-            {
-                nombre: 'pregunta_adpcion_7',
-                campo: pregunta_adpcion_7
-            },
-            {
-                nombre: 'pregunta_adpcion_8',
-                campo: pregunta_adpcion_8
-            },
-            {
-                nombre: 'pregunta_adpcion_9',
-                campo: pregunta_adpcion_9
-            },
-            {
-                nombre: 'pregunta_adpcion_10',
-                campo: pregunta_adpcion_10
-            },
-            {
-                nombre: 'pregunta_adpcion_11',
-                campo: pregunta_adpcion_11
-            },
-            {
-                nombre: 'pregunta_adpcion_12',
-                campo: pregunta_adpcion_12
-            },
-            {
-                nombre: 'pregunta_adpcion_13',
-                campo: pregunta_adpcion_13
-            },
-            {
-                nombre: 'pregunta_adpcion_14',
-                campo: pregunta_adpcion_14
-            },
-            {
-                nombre: 'pregunta_adpcion_15',
-                campo: pregunta_adpcion_15
-            },
-            {
-                nombre: 'pregunta_adpcion_16',
-                campo: pregunta_adpcion_16
-            },
-            {
-                nombre: 'pregunta_adpcion_17',
-                campo: pregunta_adpcion_17
-            },
-            {
-                nombre: 'pregunta_adpcion_18',
-                campo: pregunta_adpcion_18
-            },
-            {
-                nombre: 'terminos',
-                campo: terminos
-            },
-            {
-                nombre: 'id',
-                campo: id
-            },
+            nombre: 'nombre_adoptante',
+            campo: nombre_adoptante
+        },
+        {
+            nombre: 'direccion_adoptante',
+            campo: direccion_adoptante
+        },
+        {
+            nombre: 'id_codigo',
+            campo: id_codigo
+        },
+        {
+            nombre: 'localidad',
+            campo: localidad
+        },
+        {
+            nombre: 'telefono',
+            campo: telefono
+        },
+        {
+            nombre: 'email',
+            campo: email
+        },
+        {
+            nombre: 'ocupacion',
+            campo: ocupacion
+        },
+        {
+            nombre: 'estado_civil',
+            campo: estado_civil
+        },
+        // {
+        //     nombre: 'pregunta_mascota_1',
+        //     campo: pregunta_mascota_1
+        // },
+        // {
+        //     nombre: 'pregunta_mascota_2',
+        //     campo: pregunta_mascota_2
+        // },
+        // {
+        //     nombre: 'pregunta_mascota_3',
+        //     campo: pregunta_mascota_3
+        // },
+        // {
+        //     nombre: 'pregunta_mascota_4',
+        //     campo: pregunta_mascota_4
+        // },
+        // {
+        //     nombre: 'pregunta_familia_1',
+        //     campo: pregunta_familia_1
+        // },
+        // {
+        //     nombre: 'pregunta_familia_2',
+        //     campo: pregunta_familia_2
+        // },
+        // {
+        //     nombre: 'pregunta_familia_3',
+        //     campo: pregunta_familia_3
+        // },
+        // {
+        //     nombre: 'pregunta_familia_4',
+        //     campo: pregunta_familia_4
+        // },
+        // {
+        //     nombre: 'pregunta_familia_5',
+        //     campo: pregunta_familia_5
+        // },
+        // {
+        //     nombre: 'pregunta_familia_6',
+        //     campo: pregunta_familia_6
+        // },
+        // {
+        //     nombre: 'pregunta_familia_7',
+        //     campo: pregunta_familia_7
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_1',
+        //     campo: pregunta_adpcion_1
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_2',
+        //     campo: pregunta_adpcion_2
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_3',
+        //     campo: pregunta_adpcion_3
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_4',
+        //     campo: pregunta_adpcion_4
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_5',
+        //     campo: pregunta_adpcion_5
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_6',
+        //     campo: pregunta_adpcion_6
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_7',
+        //     campo: pregunta_adpcion_7
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_8',
+        //     campo: pregunta_adpcion_8
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_9',
+        //     campo: pregunta_adpcion_9
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_10',
+        //     campo: pregunta_adpcion_10
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_11',
+        //     campo: pregunta_adpcion_11
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_12',
+        //     campo: pregunta_adpcion_12
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_13',
+        //     campo: pregunta_adpcion_13
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_14',
+        //     campo: pregunta_adpcion_14
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_15',
+        //     campo: pregunta_adpcion_15
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_16',
+        //     campo: pregunta_adpcion_16
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_17',
+        //     campo: pregunta_adpcion_17
+        // },
+        // {
+        //     nombre: 'pregunta_adpcion_18',
+        //     campo: pregunta_adpcion_18
+        // },
+        // {
+        //     nombre: 'terminos',
+        //     campo: terminos
+        // },
+        {
+            nombre: 'id',
+            campo: id
+        },
         ];
 
         /**Se busca en el array si alguno de los campos obligatorios 
@@ -237,10 +237,10 @@ router.post("/formulario", async(req, res) => {
             });
         }
 
-        
 
 
-       
+
+
 
 
         /**Se llama a la función que hace el registro y se obtiene la respuesta, 
@@ -259,7 +259,7 @@ router.post("/formulario", async(req, res) => {
                 msg: `Ocurrió un error al guardar el formulario`
             });
         }
-        
+
         /**Sino quiere decir que la función retornó un
          * números distinto de 0 y se registro la adopción
          */
@@ -268,12 +268,12 @@ router.post("/formulario", async(req, res) => {
             /**Se busca la mascota que se acaba de crear
              * dado el id_formulario que se acaba de obtener
              */
-           // const form = await adops.obtenerPorId(id_formulario);
+            // const form = await adops.obtenerPorId(id_formulario);
 
             res.json({
                 ok: true,
                 msg: `formulario guardada exitosamente`,
-               // form
+                // form
             });
         }
 
@@ -290,7 +290,7 @@ router.post("/formulario", async(req, res) => {
 
 
 
-router.post("/formulariomascota", async(req, res) => {
+router.post("/formulariomascota", async (req, res) => {
 
     try {
         /**Se toman solo los campos necesarios 
@@ -298,9 +298,9 @@ router.post("/formulariomascota", async(req, res) => {
          * 
          */
         const {
-            id_mascota, 
+            id_mascota,
             id_formulario
-    
+
         } = req.body;
 
         /**Se guardan dentro de un array,
@@ -308,13 +308,13 @@ router.post("/formulariomascota", async(req, res) => {
          * de la petición 
          */
         const campos = [{
-                nombre: 'id_mascota',
-                campo: id_mascota
-            },
-            {
-                nombre: 'id_formulario',
-                campo: id_formulario
-            }
+            nombre: 'id_mascota',
+            campo: id_mascota
+        },
+        {
+            nombre: 'id_formulario',
+            campo: id_formulario
+        }
         ];
 
         /**Se busca en el array si alguno de los campos obligatorios 
@@ -344,7 +344,7 @@ router.post("/formulariomascota", async(req, res) => {
         /**Si la función retorna 0, quiere decir
          * que la mascota no se pudo crear
          */
-        if (id_formul=== 0) {
+        if (id_formul === 0) {
 
             res.status(500).json({
                 ok: false,
@@ -359,12 +359,12 @@ router.post("/formulariomascota", async(req, res) => {
             /**Se busca la mascota que se acaba de crear
              * dado el id_formulario que se acaba de obtener
              */
-         //  const form = await adops.obtenerPorId(id_formul);
+            //  const form = await adops.obtenerPorId(id_formul);
 
             res.json({
                 ok: true,
                 msg: `formulario guardada exitosamente`,
-               // form
+                // form
             });
         }
 
@@ -379,8 +379,8 @@ router.post("/formulariomascota", async(req, res) => {
     }
 });
 
- 
-router.post("/formulariopeticion", async(req, res) => {
+
+router.post("/formulariopeticion", async (req, res) => {
 
     try {
         /**Se toman solo los campos necesarios 
@@ -388,10 +388,10 @@ router.post("/formulariopeticion", async(req, res) => {
          * 
          */
         const {
-            solicitud_adopcion, 
+            solicitud_adopcion,
             id_mascota,
             id_formulario
-    
+
         } = req.body;
 
         /**Se guardan dentro de un array,
@@ -440,7 +440,7 @@ router.post("/formulariopeticion", async(req, res) => {
         /**Si la función retorna 0, quiere decir
          * que la mascota no se pudo crear
          */
-        if (id_formul=== 0) {
+        if (id_formul === 0) {
 
             res.status(500).json({
                 ok: false,
@@ -451,21 +451,21 @@ router.post("/formulariopeticion", async(req, res) => {
          * números distinto de 0 y se registro la adopción
          */
 
-        
+
         else {
-            
+
 
             /**Se busca la mascota que se acaba de crear
              * dado el id_formulario que se acaba de obtener
              */
-         // const form = await adops.mostarformularios(id_formulario);
-         // console.log(form)
+            // const form = await adops.mostarformularios(id_formulario);
+            // console.log(form)
 
             res.json({
                 ok: true,
                 msg: ` respuesta exitosa`,
-                
-               //form
+
+                //form
             });
         }
 
@@ -512,7 +512,7 @@ router.post("/filtro", async (req, res) => {
         }
 
         const id_formul = await adops.Filtro(req);
-        
+
         if (id_formul === null) {
 
             res.status(500).json({
