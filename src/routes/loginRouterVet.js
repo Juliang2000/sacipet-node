12f907b44ca674d83001f59c5d7f7f52cbbed8e3
julian gustavo gil hernandez <root>
2021-04-19 10:05:46 -0500

    cambio

--- src/routes/loginRouterVet.js
+++ src/routes/loginRouterVet.js
@@ -168,14 +168,14 @@ router.post("/login", async(req, res) => {
         console.log(`Ingreso_exitoso con: ${origen_cuenta}`)
 
 
-        let obj_unidos = Object.assign(dbUser, foto);
-        console.log(dbUser)
+     
 
         return res.status(200).json({
 
             ok: true,
             msg: `¡Autenticación exitosa!`,
             user: dbUser,
+            fotos:foto,
             
             tokenGenerado
         });
