68fe3a45a42c409a49ad8614ff73d7faa34bdf2f
julian gustavo gil hernandez <root>
2021-04-29 08:34:24 -0500

    preguntas respon

--- src/controllers/formulario.controller.js
+++ src/controllers/formulario.controller.js
@@ -2672,13 +2672,13 @@ const solicitudes= async (id) => {
             await pool.query(`SELECT id_mascota, nombre_mascota, edad_mascota, escala_edad, esterilizado, id_raza, id_tamanio, id_color, descripcion_mascota,
                 id_usuario, tipo_tramite, id_codigo, genero_mascota, publicado, telefono1, telefono2, correo, id_formulario, solicitud_adopcion,
                 id, nombre_adoptante, direccion_adoptante, ciudad, localidad, telefono, email, ocupacion, estado_civil, pregunta_1, pregunta_2,
-                pregunta_3, pregunta_4, pregunta_5, pregunta_6, terminos, nombres, telefono_user, correo_user,STRING_AGG(distinct id_foto, ',') fotos
+                pregunta_3, pregunta_4, pregunta_5, pregunta_6, terminos, nombres, telefono_user, correo_user,STRING_AGG(distinct id_foto, ',') fotos,raza,id_tipo_mascota
                     FROM public.v_solicitudes where id=$1
                     
                     GROUP BY id_mascota, nombre_mascota, edad_mascota, escala_edad, esterilizado, id_raza, id_tamanio, id_color, descripcion_mascota,
                 id_usuario, tipo_tramite, id_codigo, genero_mascota, publicado, telefono1, telefono2, correo, id_formulario, solicitud_adopcion,
                 id, nombre_adoptante, direccion_adoptante, ciudad, localidad, telefono, email, ocupacion, estado_civil, pregunta_1, pregunta_2,
-                pregunta_3, pregunta_4, pregunta_5, pregunta_6, terminos, nombres, telefono_user, correo_user;` , [id]);
+                pregunta_3, pregunta_4, pregunta_5, pregunta_6, terminos, nombres, telefono_user, correo_user,raza,id_tipo_mascota;` , [id]);
 
 
         if (JSON.stringify(respuesta.rows) === '[]') {
